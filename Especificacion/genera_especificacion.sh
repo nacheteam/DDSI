82cb219e55e12444a1f91a9c9ce8d9009fd5cc4d
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-10-28 18:32:42 +0200

    Añadida línea al script de generación de la especificación.

--- Especificacion/genera_especificacion.sh
+++ Especificacion/genera_especificacion.sh
@@ -2,3 +2,4 @@
 pandoc ./Subsistema\ Usuarios/requisitos_funcionales.md ./Subsistema\ Mantenimiento/requisitos_funcionales.md ./Subsistema\ Central/requisitos_funcionales.md -o requisitos_funcionales.pdf
 pandoc ./Subsistema\ Usuarios/requisitos_datos.md ./Subsistema\ Mantenimiento/requisitos_datos.md ./Subsistema\ Central/requisitos_datos.md -o requisitos_datos.pdf
 pandoc ./Subsistema\ Usuarios/restricciones_semanticas.md ./Subsistema\ Mantenimiento/restricciones_semanticas.md ./Subsistema\ Central/restricciones_semanticas.md -o restricciones_semanticas.pdf
+pandoc entrega.md -o entrega.pdf

b514be34f4f5f13f75d990c64c06b5cb33a87d49
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-09 23:12:01 +0100

    Commit para la conexion

--- Implementacion/Subsistema Mantenimiento/interfaz_mantenimiento.py
+++ Implementacion/Subsistema Mantenimiento/interfaz_mantenimiento.py
@@ -31,24 +31,27 @@ def menu():
     return opcion
 
 # Función de bicicleta averiada.
-def bicicletaAveriada(cursor):
+def bicicletaAveriada(cursor,db_connection):
     cod_bicicleta = int(raw_input("Introduzca el código de la bicicleta averiada:"))
     print("En reparación...\n")
     cursor.execute("SELECT Posicion FROM Bicicleta WHERE CodigoBicicleta=='" + str(cod_bicicleta) +"';")
     for Posicion from cursor:
         cursor.execute("UPDATE Bicicleta SET Estado='Reparacion', Posicion='Taller' WHERE CodigoBicicleta=='" + str(cod_bicicleta) + "';")
+        db_connection.commit()
         time.sleep(5)
         cursor.execute("UPDATE Bicicleta SET Estado='Disponible', Posicion='" + str(Posicion) + "' WHERE CodigoBicicleta=='" + str(cod_bicicleta) + "';")
+        db_connection.commit()
     num_mecanicos = random.randint(1,5)
     codigos_mecanicos = mecanicosAleatorios(num_mecanicos)
     taller = random.randint(1,NUM_TALLERES)
     print("Los mecánicos que han reparado la bicicleta tienen los códigos: " + str(codigos_mecanicos) + "\n")
     for mecanico in codigos_mecanicos:
         cursor.execute("INSERT INTO ReparaBicicleta (CodigoBicicleta,CodigoPersonal,NumeroTaller) VALUES ('" + str(cod_bicicleta) + "','" + str(mecanico) + "','" + str(taller) + "');")
+    db_connection.commit()
     print("¡Reparada!\n")
 
 # Función de mantenimiento.
-def mantenimientoBicicletas(cursor):
+def mantenimientoBicicletas(cursor,db_connection):
     tam_parte = NUM_BICICLETAS/5
     for i in range(5):
         posiciones=[]
@@ -57,20 +60,22 @@ def mantenimientoBicicletas(cursor):
             for posicion from cursor:
                 posiciones.append(posicion)
             cursor.execute("UPDATE Bicicleta SET Estado='Mantenimiento', Posicion='Taller' WHERE CodigoBicicleta=='" + str(j) + "';")
+            db_connection.commit()
         time.sleep(5)
         for j in range(tam_parte*i,tam_parte*i-1):
             cursor.execute("UPDATE Bicicleta SET Estado='Mantenimiento', Posicion='" + str(posiciones[j]) + "' WHERE CodigoBicicleta=='" + str(j) + "';")
+            db_connection.commit()
 
 # Función de notificación de rotura de una estación de préstamo.
-def roturaEstacion(cursor):
+def roturaEstacion(cursor,db_connection):
 
 
 # Función de traslado de bicicletas entre estaciones.
-def trasladoBicicletas(cursor):
+def trasladoBicicletas(cursor,db_connection):
 
 
 # Función de notificación de una incidencia.
-def notificacionIncidencia(cursor):
+def notificacionIncidencia(cursor,db_connection):
     codigoSiguienteIncidencia = raw_input("Introduzca el código de la incidencia: ")
     tipo = raw_input("Introduzca el tipo de incidencia: ")
     descripcion = raw_input("Introduzca una descripción: ")
@@ -78,4 +83,5 @@ def notificacionIncidencia(cursor):
     mecanico = random.randint(1,NUM_PERSONAL)
     print("El mecánico que ha notificado la incidencia es: " + str(mecanico) + "\n")
     cursor.execute("INSERT INTO Pone (CodigoPersonal,CodigoReclamacion) VALUES ('" + str(mecanico) + "','" + str(codigoSiguienteIncidencia) + "');")
+    db_connection.commit()
     print("Notificación de la incidencia realizada.\n")

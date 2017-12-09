4e9ca75ca8d8e40a246ab40e364830a7d56a4c93
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-09 13:46:45 +0100

    Corregido el script de paso a tablas

--- Implementacion/Paso a tablas/pasoatablas.sql
+++ Implementacion/Paso a tablas/pasoatablas.sql
@@ -11,33 +11,33 @@ CREATE TABLE Incidencias(
 CodigoIncidencia INT,
 Tipo VARCHAR(140),
 Descripcion VARCHAR(140),
-PRIMARY KEY CodigoIncidencia
+PRIMARY KEY(CodigoIncidencia)
 );
 
 CREATE TABLE Bicicleta(
 CodigoBicicleta INT,
 Estado VARCHAR(140),
 Posicion VARCHAR(140),
-PRIMARY KEY CodigoBicicleta
+PRIMARY KEY(CodigoBicicleta)
 );
 
 CREATE TABLE Estacion(
 CodigoEstacion INT,
 Estado VARCHAR(140),
 Posicion VARCHAR(140),
-PRIMARY KEY CodigoEstacion
+PRIMARY KEY(CodigoEstacion)
 );
 
 CREATE TABLE Banco(
 Entidad INT,
-PRIMARY KEY Entidad
+PRIMARY KEY(Entidad)
 );
 
 CREATE TABLE Reclamacion(
 CodigoReclamacion INT,
 NombreArchivo VARCHAR(140),
-Fecha VARCHAR(140)
-PRIMARY KEY CodigoReclamacion
+Fecha VARCHAR(140),
+PRIMARY KEY(CodigoReclamacion)
 );
 
 CREATE TABLE Usuario(
@@ -51,76 +51,67 @@ Telefono VARCHAR(13),
 Contrasena VARCHAR(20),
 TiempoAsignado INT,
 Sancionado BOOLEAN,
-PRIMARY KEY DNI
+PRIMARY KEY(DNI)
 );
 
 CREATE TABLE Comunica(
-CodigoIncidencia INT,
-CodigoPersonal INT NOT NULL,
-PRIMARY KEY CodigoIncidencia,
-FOREIGN KEY CodigoIncidencia, CodigoPersonal
+CodigoIncidencia INT REFERENCES Incidencia(CodigoIncidencia),
+CodigoPersonal INT NOT NULL REFERENCES Personal(CodigoPersonal),
+PRIMARY KEY(CodigoIncidencia)
 );
 
 CREATE TABLE ReparaBicicleta(
-CodigoBicicleta INT NOT NULL,
-CodigoPersonal INT NOT NULL,
+CodigoBicicleta INT NOT NULL REFERENCES Bicicleta(CodigoBicicleta),
+CodigoPersonal INT NOT NULL REFERENCES Personal(CodigoPersonal),
 NumeroTaller INT,
-PRIMARY KEY CodigoBicicleta, CodigoPersonal,
-FOREIGN KEY CodigoBicicleta, CodigoPersonal
+PRIMARY KEY(CodigoBicicleta, CodigoPersonal)
 );
 
 CREATE TABLE Traslada(
-CodigoBicicleta INT,
-CodigoPersonal INT NOT NULL,
+CodigoBicicleta INT REFERENCES Bicicleta(CodigoBicicleta),
+CodigoPersonal INT NOT NULL REFERENCES Personal(CodigoPersonal),
 EstacionPocasBicicletas INT,
 EstacionMuchasBicicletas INT,
 NumeroBicicletas INT,
-PRIMARY KEY CodigoBicicleta, CodigoPersonal,
-FOREIGN KEY CodigoBicicleta, CodigoPersonal,
+PRIMARY KEY(CodigoBicicleta, CodigoPersonal)
 );
 
 CREATE TABLE Revisa(
-CodigoBicicleta INT,
-CodigoPersonal INT,
+CodigoBicicleta INT REFERENCES Bicicleta(CodigoBicicleta),
+CodigoPersonal INT REFERENCES Personal(CodigoPersonal),
 NumeroTaller INT,
-PRIMARY KEY CodigoBicicleta, CodigoPersonal,
-FOREIGN KEY CodigoBicicleta, CodigoPersonal
+PRIMARY KEY(CodigoBicicleta, CodigoPersonal)
 );
 
 CREATE TABLE ReparaEstacion(
-CodigoEstacion INT,
-CodigoBicicleta INT,
+CodigoEstacion INT REFERENCES Estacion(CodigoEstacion),
+CodigoBicicleta INT REFERENCES Bicicleta(CodigoBicicleta),
 MensajeReparacion VARCHAR(140),
-PRIMARY KEY CodigoEstacion, CodigoBicicleta,
-FOREIGN KEY CodigoEstacion, CodigoBicicleta
+PRIMARY KEY(CodigoEstacion, CodigoBicicleta)
 );
 
 CREATE TABLE Sanciona(
-DNI CHAR(9),
-CodigoEstacion INT,
-CodigoBicicleta INT,
-PRIMARY KEY DNI, CodigoEstacion, CodigoBicicleta,
-FOREIGN KEY DNI, CodigoEstacion, CodigoBicicleta
+DNI CHAR(9) REFERENCES Usuario(DNI),
+CodigoEstacion INT REFERENCES Estacion(CodigoEstacion),
+CodigoBicicleta INT REFERENCES Bicicleta(CodigoBicicleta),
+PRIMARY KEY(DNI, CodigoEstacion, CodigoBicicleta)
 );
 
 CREATE TABLE Utiliza(
-DNI CHAR(9),
-CodigoBicicleta INT,
+DNI CHAR(9) REFERENCES Usuario(DNI),
+CodigoBicicleta INT REFERENCES Bicicleta(CodigoBicicleta),
 TiempoEmpleado INT,
-PRIMARY KEY DNI, CodigoBicicleta,
-FOREIGN KEY DNI, CodigoBicicleta
+PRIMARY KEY(DNI, CodigoBicicleta)
 );
 
 CREATE TABLE RecibePago(
-DNI CHAR(9),
-Entidad INT NOT NULL,
-PRIMARY KEY DNI,
-FOREIGN KEY DNI, Entidad
+DNI CHAR(9) REFERENCES Usuario(DNI),
+Entidad INT NOT NULL REFERENCES Banco(Entidad),
+PRIMARY KEY(DNI)
 );
 
 CREATE TABLE Pone(
-CodigoPersonal INT NOT NULL,
-CodigoReclamacion INT,
-PRIMARY KEY CodigoReclamacion,
-FOREIGN KEY CodigoReclamacion, CodigoPersonal,
+CodigoPersonal INT NOT NULL REFERENCES Personal(CodigoPersonal),
+CodigoReclamacion INT REFERENCES Reclamacion(CodigoReclamacion),
+PRIMARY KEY(CodigoReclamacion)
 );

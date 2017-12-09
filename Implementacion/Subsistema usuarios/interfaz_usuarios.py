5380cfd3108c7b896c1ba586779add131a1a1117
luisbalru <balderasruizluis@gmail.com>
2017-12-09 11:37:14 +0100

    Corrección en la toma de datos

--- Implementacion/Subsistema usuarios/interfaz_usuarios.py
+++ Implementacion/Subsistema usuarios/interfaz_usuarios.py
@@ -17,22 +17,14 @@ def menu():
 # Función para dar de alta un nuevo usuario
 def nuevoUsuario():
     print("Preparado para crear nuevo usuario.\n")
-    print("Escriba su DNI (será su user para entrar).\n")
-    dni = char(raw_input("Mi DNI es "))
-    print("Escriba su nombre completo.\n")
-    nombre = raw_input("Me llamo ")
-    print("Escriba su edad.\n")
-    edad = int(raw_input("Mi"))
-    print("Escriba su número de cuenta.\n")
-    num_cuenta = int(raw_input())
-    print("Escriba su email.\n")
-    email = raw_input()
-    print("Elija su tarifa. Tenemos: 1. Barata, 2. Cara, 3. Atraco.\n")
-    tarifa = int(raw_input())
-    print("Escriba su telefono.\n")
-    telefono = int(raw_input())
-    print("Elija una nueva contraseña para su perfil.\n")
-    contraseña = raw_input()
+    dni = raw_input("Escriba su DNI (será su user para entrar)")
+    nombre = raw_input("Escriba su nombre completo.")
+    edad = int(raw_input("Escriba su edad."))
+    num_cuenta = int(raw_input("Escriba su número de cuenta."))
+    email = raw_input("Escriba su email.")
+    tarifa = int(raw_input("Elija su tarifa. Tenemos: 1. Barata, 2. Cara, 3. Atraco."))
+    telefono = int(raw_input("Escriba su telefono."))
+    contraseña = raw_input("Elija una nueva contraseña para su perfil.")
 
 #Acceso a la base de datos y vista de la información del usuario
 def accesoPerfil():

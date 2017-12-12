9a8debc896180184f8b37883901d5b4ff7b9eca0
luisbalru <balderasruizluis@gmail.com>
2017-12-12 21:54:35 +0100

    Rellenando la tabla de tarifas y tiempos asignados...

---
+++ Implementacion/Paso a tablas/tarifas_tiempos.sql
@@ -0,0 +1,3 @@
+INSERT INTO TarifasTiempo(Tarifa,TiempoAsignado) VALUES('0','45');
+INSERT INTO TarifasTiempo(Tarifa,TiempoAsignado) VALUES('1','60');
+INSERT INTO TarifasTiempo(Tarifa,TiempoAsignado) VALUES('2','90');

2203f73a0e74b4292b22ee8af85494725d5db3db
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-18 17:06:21 +0100

    Base de datos fresquita fresquita

--- Implementacion/bicicletasParis.sql
+++ Implementacion/bicicletasParis.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.14  Distrib 5.5.57-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.16  Distrib 10.2.11-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: BicicletasParis
 -- ------------------------------------------------------
--- Server version	5.5.57-MariaDB-1ubuntu0.14.04.1
+-- Server version	10.2.11-MariaDB-10.2.11+maria~xenial
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -58,7 +58,7 @@ CREATE TABLE `Bicicleta` (
 
 LOCK TABLES `Bicicleta` WRITE;
 /*!40000 ALTER TABLE `Bicicleta` DISABLE KEYS */;
-INSERT INTO `Bicicleta` VALUES (0,'Activa','8'),(1,'Activa','19'),(2,'Activa','15'),(3,'Activa','24'),(4,'Activa','25'),(5,'Activa','13'),(6,'Activa','16'),(7,'Activa','7'),(8,'Activa','8'),(9,'Activa','25'),(10,'Activa','27'),(11,'Activa','30'),(12,'Activa','5'),(13,'Activa','10'),(14,'Activa','0'),(15,'Activa','19'),(16,'Activa','8'),(17,'Activa','3'),(18,'Activa','10'),(19,'Activa','21'),(20,'Activa','6'),(21,'Activa','15'),(22,'Activa','24'),(23,'Activa','25'),(24,'Activa','19'),(25,'Activa','20'),(26,'Activa','8'),(27,'Activa','5'),(28,'Activa','0'),(29,'Activa','18'),(30,'Activa','22'),(31,'Activa','29'),(32,'Activa','17'),(33,'Activa','26'),(34,'Activa','26'),(35,'Activa','5'),(36,'Activa','24'),(37,'Activa','27'),(38,'Activa','25'),(39,'Activa','24'),(40,'Activa','11'),(41,'Activa','30'),(42,'Activa','4'),(43,'Activa','18'),(44,'Activa','1'),(45,'Activa','27'),(46,'Activa','23'),(47,'Activa','5'),(48,'Activa','0'),(49,'Activa','29'),(50,'Activa','24'),(51,'Activa','22'),(52,'Activa','2'),(53,'Activa','2'),(54,'Activa','4'),(55,'Activa','27'),(56,'Activa','5'),(57,'Activa','15'),(58,'Activa','4'),(59,'Activa','3'),(60,'Activa','15'),(61,'Activa','8'),(62,'Activa','18'),(63,'Activa','11'),(64,'Activa','15'),(65,'Activa','0'),(66,'Activa','17'),(67,'Activa','17'),(68,'Activa','16'),(69,'Activa','14'),(70,'Activa','25'),(71,'Activa','3'),(72,'Activa','26'),(73,'Activa','14'),(74,'Activa','13'),(75,'Activa','14'),(76,'Activa','5'),(77,'Activa','11'),(78,'Activa','24'),(79,'Activa','20'),(80,'Activa','29'),(81,'Activa','9'),(82,'Activa','23'),(83,'Activa','15'),(84,'Activa','20'),(85,'Activa','10'),(86,'Activa','11'),(87,'Activa','4'),(88,'Activa','8'),(89,'Activa','20'),(90,'Activa','26'),(91,'Activa','4'),(92,'Activa','5'),(93,'Activa','1'),(94,'Activa','20'),(95,'Activa','23'),(96,'Activa','1'),(97,'Activa','8'),(98,'Activa','3'),(99,'Activa','23'),(100,'Activa','29'),(101,'Activa','23'),(102,'Activa','6'),(103,'Activa','12'),(104,'Activa','0'),(105,'Activa','17'),(106,'Activa','28'),(107,'Activa','23'),(108,'Activa','11'),(109,'Activa','12'),(110,'Activa','6'),(111,'Activa','1'),(112,'Activa','5'),(113,'Activa','26'),(114,'Activa','10'),(115,'Activa','19'),(116,'Activa','11'),(117,'Activa','12'),(118,'Activa','25'),(119,'Activa','16'),(120,'Activa','27'),(121,'Activa','1'),(122,'Activa','0'),(123,'Activa','13'),(124,'Activa','21'),(125,'Activa','20'),(126,'Activa','16'),(127,'Activa','8'),(128,'Activa','1'),(129,'Activa','19'),(130,'Activa','29'),(131,'Activa','21'),(132,'Activa','30'),(133,'Activa','14'),(134,'Activa','15'),(135,'Activa','24'),(136,'Activa','14'),(137,'Activa','2'),(138,'Activa','9'),(139,'Activa','4'),(140,'Activa','21'),(141,'Activa','19'),(142,'Activa','24'),(143,'Activa','9'),(144,'Activa','19'),(145,'Activa','26'),(146,'Activa','5'),(147,'Activa','23'),(148,'Activa','2'),(149,'Activa','24'),(150,'Activa','25'),(151,'Activa','25'),(152,'Activa','20'),(153,'Activa','30'),(154,'Activa','3'),(155,'Activa','26'),(156,'Activa','11'),(157,'Activa','21'),(158,'Activa','29'),(159,'Activa','15'),(160,'Activa','18'),(161,'Activa','1'),(162,'Activa','13'),(163,'Activa','22'),(164,'Activa','23'),(165,'Activa','28'),(166,'Activa','27'),(167,'Activa','17'),(168,'Activa','8'),(169,'Activa','12'),(170,'Activa','22'),(171,'Activa','19'),(172,'Activa','18'),(173,'Activa','17'),(174,'Activa','11'),(175,'Activa','7'),(176,'Activa','6'),(177,'Activa','30'),(178,'Activa','20'),(179,'Activa','8'),(180,'Activa','30'),(181,'Activa','5'),(182,'Activa','12'),(183,'Activa','3'),(184,'Activa','3'),(185,'Activa','29'),(186,'Activa','17'),(187,'Activa','22'),(188,'Activa','8'),(189,'Activa','22'),(190,'Activa','8'),(191,'Activa','26'),(192,'Activa','11'),(193,'Activa','1'),(194,'Activa','23'),(195,'Activa','2'),(196,'Activa','16'),(197,'Activa','17'),(198,'Activa','17'),(199,'Activa','13');
+INSERT INTO `Bicicleta` VALUES (0,'Disponible','8'),(1,'Disponible','19'),(2,'Disponible','15'),(3,'Disponible','24'),(4,'Disponible','25'),(5,'Disponible','13'),(6,'Disponible','16'),(7,'Disponible','7'),(8,'Disponible','8'),(9,'Disponible','25'),(10,'Disponible','27'),(11,'Disponible','30'),(12,'Disponible','5'),(13,'Disponible','10'),(14,'Disponible','0'),(15,'Disponible','19'),(16,'Disponible','8'),(17,'Disponible','3'),(18,'Disponible','10'),(19,'Disponible','21'),(20,'Disponible','6'),(21,'Disponible','15'),(22,'Disponible','24'),(23,'Disponible','25'),(24,'Disponible','19'),(25,'Disponible','20'),(26,'Disponible','8'),(27,'Disponible','5'),(28,'Disponible','0'),(29,'Disponible','18'),(30,'Disponible','22'),(31,'Disponible','29'),(32,'Disponible','17'),(33,'Disponible','26'),(34,'Disponible','26'),(35,'Disponible','5'),(36,'Disponible','24'),(37,'Disponible','27'),(38,'Disponible','25'),(39,'Disponible','24'),(40,'Disponible','11'),(41,'Disponible','30'),(42,'Disponible','4'),(43,'Disponible','18'),(44,'Disponible','1'),(45,'Disponible','27'),(46,'Disponible','23'),(47,'Disponible','5'),(48,'Disponible','0'),(49,'Disponible','29'),(50,'Disponible','24'),(51,'Disponible','22'),(52,'Disponible','2'),(53,'Disponible','2'),(54,'Disponible','4'),(55,'Disponible','27'),(56,'Disponible','5'),(57,'Disponible','15'),(58,'Disponible','4'),(59,'Disponible','3'),(60,'Disponible','15'),(61,'Disponible','8'),(62,'Disponible','18'),(63,'Disponible','11'),(64,'Disponible','15'),(65,'Disponible','0'),(66,'Disponible','17'),(67,'Disponible','17'),(68,'Disponible','16'),(69,'Disponible','14'),(70,'Disponible','25'),(71,'Disponible','3'),(72,'Disponible','26'),(73,'Disponible','14'),(74,'Disponible','13'),(75,'Disponible','14'),(76,'Disponible','5'),(77,'Disponible','11'),(78,'Disponible','24'),(79,'Disponible','20'),(80,'Disponible','29'),(81,'Disponible','9'),(82,'Disponible','23'),(83,'Disponible','15'),(84,'Disponible','20'),(85,'Disponible','10'),(86,'Disponible','11'),(87,'Disponible','4'),(88,'Disponible','8'),(89,'Disponible','20'),(90,'Disponible','26'),(91,'Disponible','4'),(92,'Disponible','5'),(93,'Disponible','1'),(94,'Disponible','20'),(95,'Disponible','23'),(96,'Disponible','1'),(97,'Disponible','8'),(98,'Disponible','3'),(99,'Disponible','23'),(100,'Disponible','29'),(101,'Disponible','23'),(102,'Disponible','6'),(103,'Disponible','12'),(104,'Disponible','0'),(105,'Disponible','17'),(106,'Disponible','28'),(107,'Disponible','23'),(108,'Disponible','11'),(109,'Disponible','12'),(110,'Disponible','6'),(111,'Disponible','1'),(112,'Disponible','5'),(113,'Disponible','26'),(114,'Disponible','10'),(115,'Disponible','19'),(116,'Disponible','11'),(117,'Disponible','12'),(118,'Disponible','25'),(119,'Disponible','16'),(120,'Disponible','27'),(121,'Disponible','1'),(122,'Disponible','0'),(123,'Disponible','13'),(124,'Disponible','21'),(125,'Disponible','20'),(126,'Disponible','16'),(127,'Disponible','8'),(128,'Disponible','1'),(129,'Disponible','19'),(130,'Disponible','29'),(131,'Disponible','21'),(132,'Disponible','30'),(133,'Disponible','14'),(134,'Disponible','15'),(135,'Disponible','24'),(136,'Disponible','14'),(137,'Disponible','2'),(138,'Disponible','9'),(139,'Disponible','4'),(140,'Disponible','21'),(141,'Disponible','19'),(142,'Disponible','24'),(143,'Disponible','9'),(144,'Disponible','19'),(145,'Disponible','26'),(146,'Disponible','5'),(147,'Disponible','23'),(148,'Disponible','2'),(149,'Disponible','24'),(150,'Disponible','25'),(151,'Disponible','25'),(152,'Disponible','20'),(153,'Disponible','30'),(154,'Disponible','3'),(155,'Disponible','26'),(156,'Disponible','11'),(157,'Disponible','21'),(158,'Disponible','29'),(159,'Disponible','15'),(160,'Disponible','18'),(161,'Disponible','1'),(162,'Disponible','13'),(163,'Disponible','22'),(164,'Disponible','23'),(165,'Disponible','28'),(166,'Disponible','27'),(167,'Disponible','17'),(168,'Disponible','8'),(169,'Disponible','12'),(170,'Disponible','22'),(171,'Disponible','19'),(172,'Disponible','18'),(173,'Disponible','17'),(174,'Disponible','11'),(175,'Disponible','7'),(176,'Disponible','6'),(177,'Disponible','30'),(178,'Disponible','20'),(179,'Disponible','8'),(180,'Disponible','30'),(181,'Disponible','5'),(182,'Disponible','12'),(183,'Disponible','3'),(184,'Disponible','3'),(185,'Disponible','29'),(186,'Disponible','17'),(187,'Disponible','22'),(188,'Disponible','8'),(189,'Disponible','22'),(190,'Disponible','8'),(191,'Disponible','26'),(192,'Disponible','11'),(193,'Disponible','1'),(194,'Disponible','23'),(195,'Disponible','2'),(196,'Disponible','16'),(197,'Disponible','17'),(198,'Disponible','17'),(199,'Disponible','13');
 /*!40000 ALTER TABLE `Bicicleta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -156,6 +156,33 @@ LOCK TABLES `Llega` WRITE;
 /*!40000 ALTER TABLE `Llega` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Llega` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER revisaInsertaLlegada
+BEFORE INSERT ON Llega
+FOR EACH ROW
+BEGIN
+    IF (NEW.CodigoBicicleta<0 OR NEW.CodigoBicicleta>199) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT='Código de bicicleta fuera de los límites [0,199]';
+    END IF;
+    IF (NEW.CodigoEstacion<0 OR NEW.CodigoEstacion>29) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT='Código de estacion fuera de los límites [0,29]';
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `Personal`
@@ -310,11 +337,11 @@ DROP TABLE IF EXISTS `ReparaEstacion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ReparaEstacion` (
-  `CodigoEstacion` int(11) DEFAULT NULL,
+  `Posicion` varchar(140) NOT NULL,
   `CodigoPersonal` int(11) NOT NULL,
   `MensajeReparacion` varchar(140) DEFAULT NULL,
   `CodReparacionEstacion` varchar(140) NOT NULL,
-  PRIMARY KEY (`CodigoPersonal`,`CodReparacionEstacion`)
+  PRIMARY KEY (`CodReparacionEstacion`,`Posicion`,`CodigoPersonal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -411,13 +438,13 @@ DROP TABLE IF EXISTS `Usuario`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Usuario` (
   `DNI` char(9) NOT NULL,
-  `Nombre` varchar(20) DEFAULT NULL,
+  `Nombre` varchar(140) DEFAULT NULL,
   `Edad` int(11) DEFAULT NULL,
   `NumeroCuenta` int(11) DEFAULT NULL,
-  `Email` varchar(20) DEFAULT NULL,
+  `Email` varchar(140) DEFAULT NULL,
   `Tarifa` int(11) DEFAULT NULL,
-  `Telefono` int(13) DEFAULT NULL,
-  `Contrasena` varchar(20) DEFAULT NULL,
+  `Telefono` int(11) DEFAULT NULL,
+  `Contrasena` varchar(140) DEFAULT NULL,
   `Sancionado` tinyint(1) DEFAULT NULL,
   `KmRecorridos` int(11) DEFAULT NULL,
   PRIMARY KEY (`DNI`)
@@ -430,7 +457,6 @@ CREATE TABLE `Usuario` (
 
 LOCK TABLES `Usuario` WRITE;
 /*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
-INSERT INTO `Usuario` VALUES ('77380234S','Luis Baldo',21,23456,'bandolero@gmail.com',0,2147483647,'ddsi',0,0),('77397016S','Diego Asterio de Zab',21,23456,'diegoasterio.z@gmail',0,2147483647,'ddsi',0,0);
 /*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -489,4 +515,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-17 21:47:56
+-- Dump completed on 2017-12-18 17:06:08

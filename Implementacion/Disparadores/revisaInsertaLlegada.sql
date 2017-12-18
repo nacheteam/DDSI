22163a27b0c152ee44dce43f872fe4164701f70e
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-18 11:45:43 +0100

    Corregido el trigger de diego

--- Implementacion/Disparadores/revisaInsertaLlegada.sql
+++ Implementacion/Disparadores/revisaInsertaLlegada.sql
@@ -5,12 +5,12 @@ BEFORE INSERT ON Llega
 FOR EACH ROW
 BEGIN
     IF (NEW.CodigoBicicleta<0 OR NEW.CodigoBicicleta>199) THEN
-        SIGNAL SQLSTATE "45000"
-        SET MESSAGE_TEXT="Código de bicicleta fuera de los límites [0,199]";
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT='Código de bicicleta fuera de los límites [0,199]';
     END IF;
     IF (NEW.CodigoEstacion<0 OR NEW.CodigoEstacion>29) THEN
-        SIGNAL SQLSTATE "45000"
-        SET MESSAGE_TEXT="Código de estacion fuera de los límites [0,29]";
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT='Código de estacion fuera de los límites [0,29]';
     END IF;
 END |
 
